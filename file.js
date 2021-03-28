d0b7004afa93503fde432c72a15f4d391c7b5163
playX <gtashnik11@gmail.com>
2021-03-28 06:25:34 +0300

    Rollback

--- file.js
+++ file.js
@@ -1,36 +1,3 @@
-function Node(left, right) {
-    this.left = left;
-    this.right = right;
+for (var i = 0; i < 100000; i += 1) {
+
 }
-let nNodes = 0;
-function makeTree(depth) {
-    //  nNodes += 1;
-    if (depth == 0) {
-        return new Node(undefined, undefined);
-    }
-
-    let n1 = makeTree(depth - 1);
-    let n2 = makeTree(depth - 1);
-    return new Node(n1, n2);
-}
-
-function populate(depth, node) {
-
-    if (depth <= 0) {
-        return;
-    }
-    depth = depth - 1;
-
-    node.left = new Node(null, null);
-    node.right = new Node(null, null);
-    populate(depth, node.left);
-    populate(depth, node.right);
-}
-
-if (!globalThis.console) {
-    globalThis.console = {
-        log: print
-    }
-}
-
-makeTree(14);
