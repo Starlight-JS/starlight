49479a82399086b861dd61f289f12594c4fb1e47
playX <gtashnik11@gmail.com>
2021-05-03 16:18:28 +0300

    Update tiny lisp

--- examples/lisp.js
+++ examples/lisp.js
@@ -56,8 +56,7 @@ function eval(x, env) {
 
     if (typeof x === "string") {
         return env.get(x)[x];
-    }
-    else if (!Array.isArray(x))
+    } else if (!Array.isArray(x))
         return x;
     else if (x[0] === 'quote')
         return x[1];
@@ -93,7 +92,6 @@ function eval(x, env) {
         }
 
         proc = exprs.shift();
-
         return proc(...exprs);
     }
 }
@@ -101,23 +99,68 @@ function Exit() {
 
 }
 
-global_env['+'] = (...args) => args[0] + args[1]
-global_env['-'] = (...args) => args[0] - args[1];
-global_env['*'] = (...args) => args[0] * args[1];
-global_env['/'] = (...args) => args[0] / args[1];
-global_env['exit'] = (...args) => { throw new Exit() }
+global_env['+'] = (...args) => args.reduce((accumulator, val) => accumulator + val, 0)
+global_env['-'] = (...args) => args.reduce((accumulator, val) => accumulator - val, 0)
+global_env['*'] = (...args) => args.reduce((accumulator, val) => accumulator * val, 1)
+global_env['/'] = (...args) => args[0] / args[1]
+global_env['exit'] = function exit() { throw new Exit() }
 global_env['<'] = (...args) => args[0] < args[1]
 global_env['>'] = (...args) => args[0] > args[1]
 global_env['<='] = (...args) => args[0] <= args[1]
-
+global_env['>='] = (...args) => args[0] >= args[1]
+global_env['eq?'] = (...args) => {
+    if (args.length <= 1)
+        return false;
+    for (let i = 0; i < args.length - 1; i++) {
+        if (args[i] != args[i + 1])
+            return false;
+    }
+    return true;
+}
+global_env['car'] = (x) => x[0]
+global_env['cdr'] = (x) => {
+    let new_arr = []
+    for (let i = 1; i < x.length; i++) {
+        new_arr[i - 1] = x[i];
+    }
+    return new_arr;
+}
+global_env['list'] = (...args) => {
+    if (args.length === 0)
+        return []
+    else return args
+}
+global_env['list?'] = (x) => Array.isArray(x)
+global_env['print'] = (...args) => { print(...args) }
+global_env['readLine'] = (prompt) => prompt ? readLine(prompt) : readLine()
+global_env['trim'] = (arg) => (arg + "").trim()
+global_env['null?'] = (item) => item ? false : true
+global_env['cons'] = (x, y) => [x, ...y]
+global_env['len'] = (x) => x.length
+global_env['sym?'] = (x) => typeof x === "string"
+global_env['parseNum'] = (x) => parseFloat(x)
+global_env['regex'] = (x, flags) => new RegExp("" + x, flags ? flags : "")
+global_env['reExec'] = (x, input) => x.exec(input)
+global_env['str'] = (...args) => {
+    let result = "";
+    for (let arg in args) {
+        result += arg;
+        result += " ";
+    }
+    return result;
+}
 function repl() {
+    print("Tiny Lisp REPL.\nType '(exit)' to exit from the REPL");
     while (true) {
         try {
-            let val = eval(readFrom(tokenize(readLine('> '))));
-            if (val)
+            let tokens = tokenize(readLine('> ').trim());
+            let ast = readFrom(tokens);
+            let val = eval(ast);
+            if (val !== undefined)
                 print(val)
         } catch (e) {
             if (e instanceof Exit) {
+                print("exit repl");
                 return;
             }
             print('Error: ', e);
--- file.js
+++ file.js
@@ -56,8 +56,7 @@ function eval(x, env) {
 
     if (typeof x === "string") {
         return env.get(x)[x];
-    }
-    else if (!Array.isArray(x))
+    } else if (!Array.isArray(x))
         return x;
     else if (x[0] === 'quote')
         return x[1];
@@ -100,26 +99,57 @@ function Exit() {
 
 }
 
-global_env['+'] = (...args) => {
-    let sum = 0;
-    for (item of args)
-        sum += item;
-
-    return sum;
-}
-global_env['-'] = (...args) => args[0] - args[1];
-global_env['*'] = (...args) => args[0] * args[1];
-global_env['/'] = (...args) => args[0] / args[1];
+global_env['+'] = (...args) => args.reduce((accumulator, val) => accumulator + val, 0)
+global_env['-'] = (...args) => args.reduce((accumulator, val) => accumulator - val, 0)
+global_env['*'] = (...args) => args.reduce((accumulator, val) => accumulator * val, 1)
+global_env['/'] = (...args) => args[0] / args[1]
 global_env['exit'] = function exit() { throw new Exit() }
 global_env['<'] = (...args) => args[0] < args[1]
 global_env['>'] = (...args) => args[0] > args[1]
 global_env['<='] = (...args) => args[0] <= args[1]
-
+global_env['>='] = (...args) => args[0] >= args[1]
+global_env['eq?'] = (...args) => {
+    if (args.length <= 1)
+        return false;
+    for (let i = 0; i < args.length - 1; i++) {
+        if (args[i] != args[i + 1])
+            return false;
+    }
+    return true;
+}
+global_env['car'] = (x) => x[0]
+global_env['cdr'] = (x) => x[1]
+global_env['list'] = (...args) => {
+    if (args.length === 0)
+        return []
+    else return args
+}
+global_env['list?'] = (x) => Array.isArray(x)
+global_env['print'] = (...args) => { print(...args) }
+global_env['readLine'] = (prompt) => prompt ? readLine(prompt) : readLine()
+global_env['trim'] = (arg) => (arg + "").trim()
+global_env['null?'] = (item) => item ? false : true
+global_env['cons'] = (x, y) => [x, ...y]
+global_env['len'] = (x) => x.length
+global_env['sym?'] = (x) => typeof x === "string"
+global_env['parseNum'] = (x) => parseFloat(x)
+global_env['regex'] = (x, flags) => new RegExp("" + x, flags ? flags : "")
+global_env['reExec'] = (x, input) => x.exec(input)
+global_env['str'] = (...args) => {
+    let result = "";
+    for (let arg in args) {
+        result += arg;
+        result += " ";
+    }
+    return result;
+}
 function repl() {
     while (true) {
         try {
-            let val = eval(readFrom(tokenize(readLine('> '))));
-            if (val)
+            let tokens = tokenize(readLine('> ').trim());
+            let ast = readFrom(tokens);
+            let val = eval(ast);
+            if (val !== undefined)
                 print(val)
         } catch (e) {
             if (e instanceof Exit) {
